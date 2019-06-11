de5f5fa230b1477109862b8d94a15331bf99a10c
emma lapalm <lillypadcrafts@gmail.com>
2019-06-11 15:56:56 -0700

    organized files

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,21 @@
+// navbar components
+
+// jumbotron components
+import React from "react";
+import "./Navbar.css";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default navbar-fixrd-top">
+                <ul>
+                    <li className="info">Try and See</li>
+                    <li className=""></li>
+                    <li className="score">Score: {this.props.score}</li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
