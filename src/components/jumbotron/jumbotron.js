42c2f895d35aecd5d34235e1a7796e7466a7e58b
emma lapalm <lillypadcrafts@gmail.com>
2019-06-11 15:45:27 -0700

    Jumbotron WIP

---
+++ src/components/jumbotron/jumbotron.js
@@ -0,0 +1,11 @@
+// jumbotron components
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+    <header className = "header">
+        <h1> Clicky Game</h1>
+        <h2> Click on each image to earn a point, but remember which ones you clicked on! If you click on the same one twice you wil lose.</h2>
+    </header>
+
+);
