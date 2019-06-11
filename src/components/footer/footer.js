8ec00b9ef92d2ee6f99c8a5f97224c1c0e16f043
emma lapalm <lillypadcrafts@gmail.com>
2019-06-11 15:40:30 -0700

    Footer WIP

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+//footer component
+
+import React from "react";
+import "./footer.css";
+
+const Footer = () => (
+    <header className = "footer">
+        <p>Created by Emma LaPalm</p>
+    </header>
+);
+
+export default Footer;
