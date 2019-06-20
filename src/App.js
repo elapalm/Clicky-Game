79ef0eec6cac5e94b2f9df41065fd53ffaaff4de
emma lapalm <lillypadcrafts@gmail.com>
2019-06-20 15:55:50 -0700

    working on app.js. will not render images

--- src/App.js
+++ src/App.js
@@ -8,17 +8,43 @@ import "./App.js";
 
 //sets state to 0 or empty
 class App extends Component {
-    state = {
-      vGames,
-      clickedImage: [],
-      score: 0
-    };
+  state = {
+    vGames,
+    clickedImage: [],
+    score: 0
+  };
 
-    //
+  //
   imageClick = event => {
     const currentImg = event.target.alt;
     const AlreadyClicked =
-      this.state.clickedIMage.indexOf(currentImg) > -1;
+      this.state.clickedImage.indexOf(currentImg) > -1;
 
+    //
+    if (AlreadyClicked) {
+      this.setState({
+        vGames: this.state.vGames.sort(function (a, b) {
+          return 1 - Math.random();
+        }),
+        clickedImage: [],
+        score: 0
+      })
+    }
+    alert("You lost. Please play agin");
 
+    //
+    () => {
+      if (this.state.score === 12) {
+        alert("Woohoo! You won!!!");
+        this.setState({
+          fish: this.state.vGames.sort(function (a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedImage: [],
+          score: 0
+        });
+      }
+    }
+  )};
+};
 export default App;
